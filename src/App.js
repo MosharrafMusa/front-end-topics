7b6fb308d7f44f3349db42f8414c83872c6dd16d
Mosharraf-Musa <mosharrafny@gmail.com>
2021-02-25 15:43:21 -0500

    react props learned

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import LearnJSX from './components/LearnJSX'
+import LearnProps from './components/LearnProps';
 
-function App() {
+
+function App(props) {
   return (
     <div className="App">
       <LearnJSX/>
+      <LearnProps age='30'/>
     </div>
   );
 }
---
+++ src/components/LearnProps.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LearnProps = (props) => {
+    return (
+        <div>
+           <h3>I am {props.age} years old</h3> 
+        </div>
+    )
+}
+
+export default LearnProps;
