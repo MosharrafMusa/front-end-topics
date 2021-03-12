c3147a2e72b54c155f89f621d23554d7175458cb
Mosharraf-Musa <mosharrafny@gmail.com>
2021-03-12 12:12:16 -0500

    finish counter with jsx

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import './App.css';
 
 // import LearnJSX from './components/LearnJSX'
 // import LearnProps from './components/LearnProps';
 import Greeting from './components/Greet'
 import Welcome from './components/Welcome'
-import Counter3 from './components/Counter3'
+import Counter from './components/Counter'
 import Moods from './components/Moods'
-import Counter4 from './components/Counter4'
+import Counter_4 from './components/Counter_4'
 
 
 function App(props) {
   
   return (
     <div className="App">
-      <Counter3/>
-      <Counter4/>
-
-      <Moods/>
+      <Counter/>
     </div>
   );
 }
---
+++ src/components/Counter_4.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Counter_4() {
+    return (
+        <div>
+            <h1>Weclome Counter 4</h1>
+        </div>
+    )
+}
+
+export default Counter_4
