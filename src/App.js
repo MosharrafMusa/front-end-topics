0e7623f8858041ae55ae55a4a0421ca659be778c
Mosharraf-Musa <mosharrafny@gmail.com>
2021-04-01 14:46:15 -0400

    useContext learned

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
 import React, {useState} from 'react'
-// import LearnJSX from './components/LearnJSX'
-// import LearnProps from './components/LearnProps';
-import Greeting from './components/Greet'
-import Welcome from './components/Welcome'
-import Counter from './components/Counter'
-import Moods from './components/Moods'
-import Counter_4 from './components/Counter_4'
-import CounterHook from './hooks/CounterHook'
+import CounterHooks2 from './hooks/CounterHooks2'
+
 
 export const ThemeContext = React.createContext()
 
-
-function App(props) {
-  const [theme, setTheme]= useState('red')
-  
+const App = () => {
+  const [theme, setTheme] = useState('red')
   return (
-    <ThemeContext.Provider value = {{backgroundColor: theme}}>
-    <div className="App">
-      <CounterHook initialCount={5}/>
-    </div>
-    <button onClick={()=>setTheme(prevTheme=>{
-      return prevTheme==='red'? 'blue': 'red'
-    })}>
-      Toggle Theme
-    </button>
+    <ThemeContext.Provider value ={{backgroundColor: theme}}>
+      <CounterHooks2 initialCount = {5}/>
+      <button onClick={()=>setTheme(prevTheme=>
+        {
+          return prevTheme==='red'?'green':'red'
+        })}>Toggle Theme</button>
     </ThemeContext.Provider>
-  );
+    
+  )
 }
 
-export default App;
+export default App
+
