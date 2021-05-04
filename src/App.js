df0f95e30c156e2a59defad85912042075c16d14
Mosharraf Musa <mosharrafny@gmail.com>
2021-05-04 12:14:51 -0400

    form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react'
+import './App.css';
+import AppContact from './components/advanceTopics/ContactForm'
 import ContactForm from './components/advanceTopics/ContactForm'
 import { SampleQuestions } from './components/advanceTopics/SampleQuestions'
 import Test1 from './components/advanceTopics/Test1'
 
 
 
-const App = () => {
- 
-  return (
+const App = () => 
+   (
     <>
     <h2>Title</h2>
+    <AppContact/>
     </>
   )
-}
+
 
 export default App
