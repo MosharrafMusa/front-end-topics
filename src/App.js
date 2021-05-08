74112d5daa44f92aa7a0ad16b719fa83fa16b53a
Mosharraf Musa <mosharrafny@gmail.com>
2021-05-08 17:40:11 -0400

    card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
-
-
 import ContactForm2 from './components/advanceTopics/ContactForm2'
+import Faq from './components/advanceTopics/Faq'
 
 
 
@@ -9,7 +8,7 @@ import ContactForm2 from './components/advanceTopics/ContactForm2'
 const App = () => 
    (
     <>
-    <ContactForm2/>
+    <Faq/>
     </>
   )
 
