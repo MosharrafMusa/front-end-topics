2a95bd911614a2382038efaac938f25a8eaeb007
Mosharraf Musa <mosharrafny@gmail.com>
2021-05-13 11:11:49 -0400

    counthook

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ContactForm2 from './components/advanceTopics/ContactForm2'
 import ContactUs from './components/advanceTopics/ContactUs'
 import Faq from './components/advanceTopics/Faq'
 import Faq2 from './components/advanceTopics/Faq2'
+import CountHook from './hooks/CountHook'
 
 
 
@@ -10,7 +11,7 @@ import Faq2 from './components/advanceTopics/Faq2'
 const App = () => 
    (
     <>
-    <ContactUs/>
+    <CountHook/>
     </>
   )
 
