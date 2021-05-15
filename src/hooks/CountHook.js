eec221999d73fef0e54592b59366754e952771d0
Mosharraf Musa <mosharrafny@gmail.com>
2021-05-14 21:28:58 -0400

    count

--- src/hooks/CountHook.js
+++ src/hooks/CountHook.js
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
 
-function Example() {
-  // Declare a new state variable, which we'll call "count"
+const CountHook = () => {
   const [count, setCount] = useState(0);
-
   return (
-    <div>
+   
+       <>
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>
         Click me
       </button>
-    </div>
-  );
+    </>
+   
+  )
 }
+
+export default CountHook
+
+
