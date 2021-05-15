85b50c5455daf03df0434c38770b49dbb4ca674a
Mosharraf Musa <mosharrafny@gmail.com>
2021-05-15 10:12:32 -0400

    conditional rendering

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './App.css';
 import './index.css';
+import {Greetings} from './App'
 
 
 
 ReactDOM.render
    (
-    <App/>,
+    <Greetings isLoggedIn={true}/>,
     document.getElementById('root')
   )
