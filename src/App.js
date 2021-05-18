14369ef5a73d89fa31bd6b076217631890965492
Mosharraf Musa <mosharrafny@gmail.com>
2021-05-18 17:25:00 -0400

    img2 added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import List from './components/conditionalRendering/List'
+import Contact from './Contact';
 
-const users = [
-  { id: '1', firstName: 'Robin', lastName: 'Wieruch' },
-  { id: '2', firstName: 'Dennis', lastName: 'Wieruch' },
-];
  
-export function App() {
+ function App() {
   return (
     <div>
-      <h1>Hello Conditional Rendering</h1>
-      <List list={users} />
+      <Contact/>
     </div>
   );
 }
- 
+
+export default App;
