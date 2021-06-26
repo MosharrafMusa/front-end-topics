9f26593181f57666f5d43a069697c1b1891ae8ff
Mosharraf Musa <mosharrafny@gmail.com>
2021-06-25 20:22:52 -0400

    map-filter-reduce-together

--- src/index.js
+++ src/index.js
@@ -1,7 +1,66 @@
 const array1 = [1, 4, 9, 16];
 
 // pass a function to map
-const map1 = array1.map((x) => x + 2);
+const map1 = array1
+  .map((x) => x + 2)
+  .filter((x) => x > 7)
+  .reduce((acc, x) => acc + x, 0);
 
 console.log(map1);
 // expected output: Array [2, 8, 18, 32]
+
+const words = [
+  "spray",
+  "limit",
+  "elite",
+  "exuberant",
+  "destruction",
+  "present",
+];
+
+const res = words.filter((word) => word.length < 6);
+console.log(res);
+
+var personnel = [
+  {
+    id: 5,
+    name: "Luke Skywalker",
+    pilotingScore: 98,
+    shootingScore: 56,
+    isForceUser: true,
+  },
+  {
+    id: 82,
+    name: "Sabine Wren",
+    pilotingScore: 73,
+    shootingScore: 99,
+    isForceUser: false,
+  },
+  {
+    id: 22,
+    name: "Zeb Orellios",
+    pilotingScore: 20,
+    shootingScore: 59,
+    isForceUser: false,
+  },
+  {
+    id: 15,
+    name: "Ezra Bridger",
+    pilotingScore: 43,
+    shootingScore: 67,
+    isForceUser: true,
+  },
+  {
+    id: 11,
+    name: "Caleb Dume",
+    pilotingScore: 71,
+    shootingScore: 85,
+    isForceUser: true,
+  },
+];
+const totalJediScore = personnel
+  .filter((person) => person.isForceUser)
+  .map((jedi) => jedi.pilotingScore + jedi.shootingScore)
+  .reduce((acc, score) => acc + score, 0);
+
+console.log(totalJediScore);
