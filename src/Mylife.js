a247ededc7ebc1bc31e4c0736f6875dc56f02688
Mosharraf Musa <mosharrafny@gmail.com>
2021-06-28 13:48:40 -0400

    must

--- src/Mylife.js
+++ src/Mylife.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Mylife = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>wel come</h1>
+    </div>
+  );
+};
 
-export default Mylife
+export default Mylife;
