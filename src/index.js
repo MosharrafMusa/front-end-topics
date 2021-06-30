f815433002c1e0088ed7937f735275d87ad82f98
Mosharraf Musa <mosharrafny@gmail.com>
2021-06-30 13:23:29 -0400

    redux

--- src/index.js
+++ src/index.js
@@ -2,6 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+// redux import
+import { createStore } from "redux";
+
+// ACTION
+const increment = () => {
+  return {
+    type: "INCREMENT",
+  };
+};
+const decrement = () => {
+  return {
+    type: "DECREMENT",
+  };
+};
+// Reducer
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+  }
+};
+
+let store = createStore(counter);
 
 ReactDOM.render(
   <React.StrictMode>
