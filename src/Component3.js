73def57d84eb2dba5c380446bf6cec54805e12cc
Mosharraf Musa <mosharrafny@gmail.com>
2021-07-05 22:27:35 -0400

    bal

--- src/Component3.js
+++ src/Component3.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Component3 = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>home </h1>
+    </div>
+  );
+};
 
-export default Component3
+export default Component3;
